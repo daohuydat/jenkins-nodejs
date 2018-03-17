d198afa1bddbe2c5ca3b2b1d070f8513d3e99d87
ddao <datdao@fortna.com>
2018-03-17 16:12:03 +0700

    edit new line

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ http.createServer(function (req, res) {
   res.write('Hello Jenkins!'); //write a response to the client
   res.end(); //end the response
 }).listen(8888); //the server object listens on port 8080
+
 console.log('Server started at port 8888');
